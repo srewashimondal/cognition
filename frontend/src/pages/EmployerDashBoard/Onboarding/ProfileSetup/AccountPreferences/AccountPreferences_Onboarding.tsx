2ece7dc2eeeee9c7367d5f7b33aef0943521b12e
addinar <raddina24@gmail.com>
2026-01-02 20:09:18 -0500

    finished employer onboarding profile setup

--- frontend/src/pages/EmployerDashBoard/Onboarding/ProfileSetup/AccountPreferences/AccountPreferences_Onboarding.tsx
+++ frontend/src/pages/EmployerDashBoard/Onboarding/ProfileSetup/AccountPreferences/AccountPreferences_Onboarding.tsx
@@ -1,11 +1,24 @@
 import './AccountPreferences_Onboarding.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Question from '../../Question/Question.tsx'
 
 export default function AccountPreferences_Onboarding() {
     const [displayName, setDisplayName] = useState("");
-    /* const [profilePicture, setProfilePicture] = useState("");
-    const [notificationPreference, setNotificationPreference] = useState(""); */
+    const [profilePicture, setProfilePicture] = useState<File | null>(null);
+    const [notificationPreference, setNotificationPreference] = useState("");
+
+    const [imagePreview, setImagePreview] = useState<string | null>(null);
+    useEffect(() => {
+        if (!profilePicture) {
+            setImagePreview(null);
+            return;
+        }
+        const url = URL.createObjectURL(profilePicture);
+        setImagePreview(url);
+
+        return () => URL.revokeObjectURL(url);
+    }, [profilePicture]);
+
 
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
@@ -19,6 +32,16 @@ export default function AccountPreferences_Onboarding() {
                 <Question question={"Display Name"} input_type={"text"} 
                 value={displayName} onChange={setDisplayName} 
                 direction={"This will be the display name on your profile."}/>
+                
+                <Question question={"Profile Picture"} input_type={"file"} 
+                value={imagePreview} onChange={setProfilePicture} 
+                direction={"You may change your profile picture now if you wish."}
+                meta={"pfp"}/>
+
+                <Question question={"Notification Preference"} input_type={"checkbox"} 
+                value={notificationPreference} onChange={setNotificationPreference} 
+                options={["In-App", "E-mail"]}
+                meta={"pfp"}/>
 
             </form>
         </div>
--- frontend/src/pages/EmployerDashBoard/Onboarding/Question/Question.tsx
+++ frontend/src/pages/EmployerDashBoard/Onboarding/Question/Question.tsx
@@ -1,6 +1,10 @@
 import './Question.css';
 import { Slider } from "@radix-ui/themes";
 import { useState } from 'react';
+import { useRef } from "react";
+import default_icon from '../../../../assets/icons/default-icon.svg';
+import icon_stroke from '../../../../assets/icons/icon-stroke.svg';
+import add_cta from '../../../../assets/icons/add-cta.svg';
 
 type QuestionProps = {
     question: string;
@@ -10,10 +14,12 @@ type QuestionProps = {
     options?: string[];
     direction?: string;
     placeholder?: string;
+    meta?: "pfp" | "none";
 };
 
-export default function Question({ question, input_type, value, onChange, options=[], direction, placeholder }: QuestionProps) {
+export default function Question({ question, input_type, value, onChange, options=[], direction, placeholder, meta }: QuestionProps) {
     const [range, setRange] = useState<[number, number]>([0, 50]);
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
 
     function renderInput() {
         switch (input_type) {
@@ -56,7 +62,7 @@ export default function Question({ question, input_type, value, onChange, option
             case "checkbox":
                 return (
                     options.map((opt) => (
-                        <label key={opt}>
+                        <label className="checkbox-option">
                             <input type="checkbox" checked={value?.includes(opt)}
                                 onChange={(e) => {
                                     const checked = e.target.checked;
@@ -67,13 +73,41 @@ export default function Question({ question, input_type, value, onChange, option
                                     );
                                 }}
                             />
-                            {opt}
+                            <span className="checkbox-box">
+                                <svg
+                                    className="check-icon"
+                                    width="12"
+                                    height="10"
+                                    viewBox="0 0 12 10"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                    <path
+                                        d="M11.293 1.40039L4 8.69336L0.707031 5.40039L1.40039 4.70703L4 7.30664L10.5996 0.707031L11.293 1.40039Z"
+                                        fill="white"
+                                        stroke="white"
+                                    />
+                                </svg>
+                            </span>
+                            <span className="checkbox-label">{opt}</span>   
                         </label>
                 )));
 
                 case "file":
                     return (
-                        <input type="file" onChange={(e) => onChange?.(e.target.files?.[0])}/>
+                        (meta === "pfp") ? 
+                        ((value) ? (<div className="pfp-upload-container">
+                            <img src={value} className="uploaded-image"/>
+                            <img src={icon_stroke} className="img-frame"/>
+                            <img src={add_cta} className="pfp-add" onClick={() => fileInputRef.current?.click()}/>
+                            <input ref={fileInputRef} type="file" accept="image/*" hidden onChange={(e) => onChange?.(e.target.files?.[0])}/>
+                        </div>) :
+                        (<div className="pfp-upload">
+                            <img src={default_icon} className="pfp-base"/>
+                            <img src={add_cta} className="pfp-add" onClick={() => fileInputRef.current?.click()}/>
+                            <input ref={fileInputRef} type="file" accept="image/*" hidden onChange={(e) => onChange?.(e.target.files?.[0])}/>
+                        </div>)) :
+                        (<input type="file" accept="image/*" onChange={(e) => onChange?.(e.target.files?.[0])}/> )
                     );
 
                 default:
