83f3dfd711590cda0e1dec07724ffea624f29528
Srewashi Mondal <mondalsrewashi2004@gmail.com>
2026-01-10 21:20:58 -0500

    added employee dashboard routing + sidebar pages

---
+++ frontend/src/pages/EmployeeDashBoard/EmployeeHome/EmployeeHome.tsx
@@ -0,0 +1,9 @@
+import './EmployeeHome.css';
+
+export default function EmployeeHome() {
+    return (
+        <div className="employee-home">
+            <h1>Employee Home</h1>
+        </div>
+    );
+}
---
+++ frontend/src/pages/EmployeeDashBoard/PerformanceDashboard/PerformanceDashboard.tsx
@@ -0,0 +1,9 @@
+import './PerformanceDashboard.css';
+
+export default function PerformanceDashboard() {
+    return (
+        <div className="performance-dashboard">
+            <h1>Performance Dashboard</h1>
+        </div>
+    );
+}
