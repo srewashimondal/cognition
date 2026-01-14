d4b1a8a68599f50dec851910e71c242d23c1867d
addinar <raddina24@gmail.com>
2026-01-14 02:53:34 -0500

    added sim lessons pg

--- frontend/src/pages/EmployeeDashBoard/SimulationModules/SimulationModules.tsx
+++ frontend/src/pages/EmployeeDashBoard/SimulationModules/SimulationModules.tsx
@@ -74,6 +74,8 @@ export default function SimulationModules() {
             <div className="modules-grid">
                 {modules.map((m) => (<ModuleCard moduleInfo={m.moduleInfo} role={"employee"} status={m.status} percent={m.percent} style={true} />))}
             </div>
+            
+            <div className="filler-space"></div>
         </div>
     );
 }
